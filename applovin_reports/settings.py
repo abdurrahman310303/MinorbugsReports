73b89ab0ccf1db7dcc80b7c88365c3a32b869f0b
Abdur Rahman <abdurrahman310303@gmail.com>
2025-04-15 18:26:46 +0500

    sdfa

--- applovin_reports/settings.py
+++ applovin_reports/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5@j%1#am%#@djq5_y0x6+7x32t$f69s%-(tj)d47n40f)@8y+8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
